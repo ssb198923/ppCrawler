1effc2b9f271a9bbc2ec355efd3f31cb2792f1cc
Subin Seok <ssb198923@gmail.com>
2022-03-03 23:47:58 +0900

    euc-kr decode

--- app.js
+++ app.js
@@ -8,16 +8,14 @@ const getHtml = async () => {
         url: "https://www.ppomppu.co.kr/zboard/zboard.php?id=ppomppu&page_num=20&category=&search_type=sub_memo&keyword=hmall", 
         method: "GET", 
         responseType: "arraybuffer"
-      }).then(function (response) {
-        const decoded = iconv.decode(text,'EUC-KR');
       }).catch(function (err) { console.error(err) })
 }
 
 getHtml()
     .then(html => {
-        log(html);
+        const decoded = iconv.decode(html.data,'EUC-KR');
         let trList = [];
-        const $ = cheerio.load(html.data);
+        const $ = cheerio.load(decoded);
         const $list = $("table#revolution_main_table tbody").children("tr[class^='list']:not(.list_notice)");
 
         $list.each(function(idx, elem){
